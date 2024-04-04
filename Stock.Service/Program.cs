ef880c722acb2348e38a6cc5ada7382b78a5a80b
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-04-04 14:30:09 +0300

    Proje, Outbox ve inbox patterne ihtiyaç olduğu hale getirildi.

--- Stock.Service/Program.cs
+++ Stock.Service/Program.cs
@@ -1,5 +1,20 @@
+using MassTransit;
+using Shared;
+using Stock.Service.Consumers;
+
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.AddConsumer<OrderCreatedEventConsumer>();
+
+    configurator.UsingRabbitMq((context, _configure) =>
+    {
+        _configure.Host(builder.Configuration["RabbitMq"]);
+
+        _configure.ReceiveEndpoint(RabbitMqSettings.Stock_OrderCreatedEvent, e => e.ConfigureConsumer<OrderCreatedEventConsumer>(context));
+    });
+});
 
 var host = builder.Build();
 host.Run();
