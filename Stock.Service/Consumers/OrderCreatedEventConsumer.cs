3e5bd3e1de913bbd774f57904a772769b4ddc66d
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-04-05 08:53:02 +0300

    Stock.Service için OrderCreatedEventConsumer inbox pattern'e uygun olarak güncellendi.

--- Stock.Service/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.Service/Consumers/OrderCreatedEventConsumer.cs
@@ -1,14 +1,37 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Shared.Events;
+using Stock.Service.Models.Contexts;
+using Stock.Service.Models.Entities;
 using System.Text.Json;
 
 namespace Stock.Service.Consumers
 {
-    public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
+    public class OrderCreatedEventConsumer(StockDbContext stockDbContext) : IConsumer<OrderCreatedEvent>
     {
         public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
         {
-            await Console.Out.WriteLineAsync(JsonSerializer.Serialize(context.Message));
+            //Gelen veriyi inbox pattern gereği stock servis db'sine kaydediyoruz.
+            await stockDbContext.OrderInboxes.AddAsync(new()
+            {
+                Processed = false,
+                Payload = JsonSerializer.Serialize(context.Message)
+            });
+
+            await stockDbContext.SaveChangesAsync();
+
+            //Gelen veriyi işleyip stok güncellemesi yapılabilir.
+            List<OrderInbox> orderInboxes = await stockDbContext.OrderInboxes.Where(x => x.Processed == false).ToListAsync();
+
+            foreach (var orderInbox in orderInboxes)
+            {
+                OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderInbox.Payload);
+
+                await Console.Out.WriteLineAsync($"{orderCreatedEvent.OrderId} order id değerine karşılık olan siparişin stok işlemleri başarıyla tamamlanmıştır");
+                orderInbox.Processed = true;
+
+                await stockDbContext.SaveChangesAsync();
+            }
         }
     }
 }
